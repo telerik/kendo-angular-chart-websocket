768341470b508ebe92e85f6b2d8b7fcc684041e6
dtopalov <dimiter.topalov79@gmail.com>
2018-12-04 17:39:54 +0200

    chore: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# kendo-angular-chart-websocket
-Sample WebSocket + Kendo UI for Angular Charts application
+# WebsocketCharts
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.2.
+
+This repository holds the source code for the article on [getting started](https://www.telerik.com/kendo-angular-ui/components/charts/how-to/bind-to-websockets/) with the Kendo UI for Angular suite.
+
+## Development server
+
+Run `npm start` to build the Angular application and start the `Node.js` server
---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { WebSocketService } from './websocket.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public chartValues = [];
+  public messages = [];
+  public min: Date = new Date();
+  public max: Date = new Date(this.min.getTime() + 20000);
+  private url = 'ws://localhost:8088';
+
+  constructor(private wsService: WebSocketService) {
+    wsService.createObservableSocket(this.url)
+      .subscribe(m => {
+        const item: any = JSON.parse(m);
+        item.time = new Date(item.time);
+        if (item.value) {
+          this.chartValues = [...this.chartValues, item];
+          if (this.chartValues.length > 20) {
+            this.min = this.chartValues[this.chartValues.length - 20].time;
+            this.max = item.time;
+          }
+        } else {
+          this.messages = [...this.messages, item];
+        }
+      });
+  }
+
+  public sendMessage(message: string): void {
+    this.wsService.sendMessage(message);
+  }
+}
